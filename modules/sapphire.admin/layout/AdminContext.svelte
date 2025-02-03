96144c05903ef5e656650cc04824755b31896d5d
Gergely Laborci <gergely@laborci.hu>
2025-02-03 11:47:56 +0100

    drawer container fix

--- modules/sapphire.admin/layout/AdminContext.svelte
+++ modules/sapphire.admin/layout/AdminContext.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import type {Snippet} from "svelte";
 	import {DialogContainer, PopupContainer, ToastContainer, ToolTipContainer} from "@nano-forge/ui-tools";
-	import {DrawerContainer} from "../drawer";
+	import {DrawerContainer} from "../drawer/DrawerContainer.svelte";
 	import {getEventManager} from "../event-manager.svelte";
 	import {keyStrokeHandler} from "../key-stroke-handler";
 	let {children}: { children: Snippet } = $props();
