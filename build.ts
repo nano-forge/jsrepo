55a91c943f19a5dfd378d81b87b35fdb64babfe2
Gergely Laborci <gergely@laborci.hu>
2025-02-03 11:46:21 +0100

    build fix

--- build.ts
+++ build.ts
@@ -5,11 +5,9 @@ const modulesDirectory = fs.realpathSync("modules");
 
 fs.readdirSync(modulesDirectory).forEach(module => {
 	if (fs.lstatSync(`${modulesDirectory}/${module}`).isDirectory()) {
-		fs.readdirSync(`${modulesDirectory}/${module}`).forEach(submodule => {
-			if (fs.lstatSync(`${modulesDirectory}/${module}/${submodule}`).isDirectory()) {
-				buildModule(`${modulesDirectory}/${module}/${submodule}`);
-			}
-		})
+		if (fs.lstatSync(`${modulesDirectory}/${module}`).isDirectory()) {
+			buildModule(`${modulesDirectory}/${module}`);
+		}
 	}
 });
 
@@ -59,50 +57,50 @@ function generateExportStatementsRecursive(dir: string, baseDir: string): string
 
 	return [...fileStatements, ...dirStatements].join('\n');
 }
-
-function collectImports(dir: string): void {
-	const collectedImports = traverseDirectory(dir, dir);
-	const uniqueImports = Array.from(new Set(collectedImports));
-
-	const importStatements = uniqueImports.sort()
-		.map(importPath => `${importPath}`)
-		.join('\n');
-
-	fs.writeFileSync(path.join(dir, "imports.txt"), importStatements);
-}
-function findImports(filePath: string, rootDir: string): string[] {
-	const content = fs.readFileSync(filePath, 'utf-8');
-	const importRegex = /import\s+.*\s+from\s+['"](.*)['"]/g;
-	const imports: string[] = [];
-	let match;
-
-	while ((match = importRegex.exec(content)) !== null) {
-		const importPath = match[1];
-		const absoluteImportPath = path.resolve(path.dirname(filePath), importPath);
-		if (!importPath.startsWith('.')) imports.push('npm: ' + importPath);
-		// else if (path.relative(rootDir, absoluteImportPath).startsWith('..')) {
-		// 	imports.push('@gm: ' + path.relative(modulesDirectory, absoluteImportPath));
-		// }
-	}
-
-	return imports;
-}
-function traverseDirectory(dir: string, rootDir: string): string[] {
-	const files = fs.readdirSync(dir);
-	const fileStatements: string[] = [];
-	const dirStatements: string[] = [];
-
-	files.forEach(file => {
-		const filePath = path.join(dir, file);
-		if (fs.lstatSync(filePath).isDirectory()) {
-			const folderStatements = traverseDirectory(filePath, rootDir);
-			if (folderStatements.length > 0) {
-				dirStatements.push(...folderStatements);
-			}
-		} else if (file.endsWith('.ts') || file.endsWith('.svelte')) {
-			fileStatements.push(...findImports(filePath, rootDir));
-		}
-	});
-
-	return [...fileStatements, ...dirStatements];
-}
+//
+// function collectImports(dir: string): void {
+// 	const collectedImports = traverseDirectory(dir, dir);
+// 	const uniqueImports = Array.from(new Set(collectedImports));
+//
+// 	const importStatements = uniqueImports.sort()
+// 		.map(importPath => `${importPath}`)
+// 		.join('\n');
+//
+// 	fs.writeFileSync(path.join(dir, "imports.txt"), importStatements);
+// }
+// function findImports(filePath: string, rootDir: string): string[] {
+// 	const content = fs.readFileSync(filePath, 'utf-8');
+// 	const importRegex = /import\s+.*\s+from\s+['"](.*)['"]/g;
+// 	const imports: string[] = [];
+// 	let match;
+//
+// 	while ((match = importRegex.exec(content)) !== null) {
+// 		const importPath = match[1];
+// 		const absoluteImportPath = path.resolve(path.dirname(filePath), importPath);
+// 		if (!importPath.startsWith('.')) imports.push('npm: ' + importPath);
+// 		// else if (path.relative(rootDir, absoluteImportPath).startsWith('..')) {
+// 		// 	imports.push('@gm: ' + path.relative(modulesDirectory, absoluteImportPath));
+// 		// }
+// 	}
+//
+// 	return imports;
+// }
+// function traverseDirectory(dir: string, rootDir: string): string[] {
+// 	const files = fs.readdirSync(dir);
+// 	const fileStatements: string[] = [];
+// 	const dirStatements: string[] = [];
+//
+// 	files.forEach(file => {
+// 		const filePath = path.join(dir, file);
+// 		if (fs.lstatSync(filePath).isDirectory()) {
+// 			const folderStatements = traverseDirectory(filePath, rootDir);
+// 			if (folderStatements.length > 0) {
+// 				dirStatements.push(...folderStatements);
+// 			}
+// 		} else if (file.endsWith('.ts') || file.endsWith('.svelte')) {
+// 			fileStatements.push(...findImports(filePath, rootDir));
+// 		}
+// 	});
+//
+// 	return [...fileStatements, ...dirStatements];
+// }
